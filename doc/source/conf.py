17eb8bbe30aa7dca8b17f4855b2371b1b582895f
Thijs Triemstra <info@collab.nl>
2016-04-05 18:07:46 +0200

    fix copyright year

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 import os
+from datetime import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -75,7 +76,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'OdooRPC'
-copyright = u'2014, Sébastien Alix'
+copyright = u'2014-{0}, Sébastien Alix'.format(datetime.now().year)
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
