8c9daa0e049579cfe7afd255b578d6c33e6e05be
sebalix <seb@usr-src.org>
2016-11-08 09:27:48 +0100

    [IMP] Bump version to 0.5.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from distutils.core import setup
 
 name = 'OdooRPC'
-version = '0.4.3'
+version = '0.5.0'
 description = ("OdooRPC is a Python module providing an easy way to "
                "pilot your Odoo servers through RPC.")
 keywords = ("openerp odoo server rpc client xml-rpc xmlrpc jsonrpc json-rpc "
