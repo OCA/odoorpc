24ee999a24b9ee1b96055a93e6e2acb2d7da2597
sebalix <seb@usr-src.org>
2017-04-09 10:46:27 +0200

    [IMP] setup.py - Add Python 3.6 and the new 'Framework :: Odoo' classifier

--- setup.py
+++ setup.py
@@ -63,10 +63,12 @@ setup(name=name,
           "Programming Language :: Python :: 3.3",
           "Programming Language :: Python :: 3.4",
           "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: Implementation :: CPython",
           "Programming Language :: Python :: Implementation :: PyPy",
           "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
           "Topic :: Software Development :: Libraries :: Python Modules",
+          "Framework :: Odoo",
       ],
       )
 
