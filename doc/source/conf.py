70a68960b3b530a7426039726fa583a7078ebd10
sebalix <seb@usr-src.org>
2018-01-15 15:39:44 +0100

    [FIX] Sphinx documentation: Could not import extension sphinx_ext.doctest_custom (required to fix builds on readthedocs.org, #16)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,6 +20,7 @@ from datetime import datetime
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('./../..'))  # To find 'sphinx_ext' package
 
 # -- General configuration ------------------------------------------------
 
