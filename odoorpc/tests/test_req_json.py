743cb411ada368efc174586d87df13098226515e
sebalix <seb@usr-src.org>
2018-08-27 14:48:50 +0200

    [FIX] Add tests for the 'ODOO.http(...)' and 'ODOO.json(...)' methods

---
+++ odoorpc/tests/test_req_json.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+
+from odoorpc.tests import BaseTestCase
+
+
+class TestReqJSON(BaseTestCase):
+
+    def _req_json(self, url):
+        data = self.odoo.json(
+            url,
+            {'db': self.env['db'],
+             'login': self.env['user'],
+             'password': self.env['pwd']})
+        self.assertEqual(data['result']['db'], self.env['db'])
+        self.assertTrue(data['result']['uid'])
+        self.assertEqual(data['result']['username'], self.env['user'])
+
+    def test_req_json_with_leading_slash(self):
+        self._req_json('/web/session/authenticate')
+
+    def test_req_json_without_leading_slash(self):
+        self._req_json('web/session/authenticate')
